f856ee8c255300f32926ba33d36f435612222182
Thomas Brockington <xtombrock1989@gmail.com>
2025-06-10 23:00:11 +0100

    save

--- src/components/home/HomePageMainContainer.jsx
+++ src/components/home/HomePageMainContainer.jsx
@@ -1,38 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+// Components
 
 function HomePageMainContainer() {
-  const [offsetY, setOffsetY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setOffsetY(window.scrollY);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
-    <main role="main" className="grid w-full h-full py-8 overflow-hidden">
-      {/* Parallax 1 */}
-      <section className="relative grid min-h-screen">
-        <div
-          className="absolute min-h-screen w-full h-full bg-parallax1 bg-cover bg-center"
-          style={{
-            transform: `translateY(${offsetY * 0.5}px)`,
-          }}
-        />
-        <div className="grid items-center justify-center text-2xl font-bold text-colour1 h-full">
+    <main role='main' className='grid w-full h-full'>
+      <section className='grid min-h-screen bg-parallax1 bg-no-repeat bg-cover bg-center bg-fixed'>
+        <div className='grid items-center justify-center text-2xl font-bold text-colour1'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, hic?
         </div>
       </section>
 
-      {/* Parallax 2 */}
-      <section className="relative grid min-h-screen">
-        <div
-          className="min-h-screen w-full h-full bg-parallax2 bg-cover"
-        />
-        <div className="grid items-center justify-center text-2xl font-bold text-colour1 h-full">
+      <section className='grid min-h-screen bg-parallax2 bg-no-repeat bg-cover bg-center'>
+        <div className='grid items-center justify-center text-2xl font-bold text-colour1'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, hic?
         </div>
       </section>
