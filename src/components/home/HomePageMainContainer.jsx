d6d8b67a651c2d8b67dd766cb128bb932a150087
Thomas Brockington <xtombrock1989@gmail.com>
2025-06-11 16:18:31 +0100

    save

--- src/components/home/HomePageMainContainer.jsx
+++ src/components/home/HomePageMainContainer.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 function HomePageMainContainer() {
   return (
     <main role='main' className='grid w-full h-full'>
-      <section className='grid min-h-screen bg-parallax1 bg-no-repeat bg-cover bg-center bg-fixed'>
+      <section className='grid min-h-screen bg-blue-500/30 bg-no-repeat bg-cover bg-center'>
+      {/* <section className='grid min-h-screen bg-parallax1 bg-no-repeat bg-cover bg-center'> */}
         <div className='grid items-center justify-center text-2xl font-bold text-colour1'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, hic?
         </div>
